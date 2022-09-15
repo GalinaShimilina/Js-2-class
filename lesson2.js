869bf4d7a5d827d5b48afcfa98a1ffcb692c5a23
Galina Shumilina <9204019@mail.rugit config --global user.name Galina>
2022-09-15 19:08:30 +0300

    first commit

---
+++ lesson2.js
@@ -0,0 +1,16 @@
+// function GetMaximum(a,b,c){
+// if(a>=b && a>=c){
+//     return a
+// }
+// else if(b>=a && b>=c ){
+//     return b
+// }
+// else{
+//     return c
+// }
+// }
+// console.log(GetMaximum(3,2,1))
+// console.log(GetMaximum(3,2,8))
+// console.log(GetMaximum(3,8,1))
+
+// const sortedTrio =
